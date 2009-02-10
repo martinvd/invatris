bbfe6895fbded48c1f142acb71f7fb3d0b25e112
martinvd <martinvdpub@gmail.com>
2009-02-10 23:11:21 +0000

    File with constants added.

---
+++ include/const.h
@@ -0,0 +1,12 @@
+#ifndef __CONST_H
+#define __CONST_H
+
+#define MAP_WIDTH 15
+#define MAP_HEIGHT 25
+#define TILE_SIZE 16
+
+#define TILE_EMPTY 0
+#define TILE_FILLED 1
+#define TILE_MOVABLE 2
+
+#endif // __CONST_H
